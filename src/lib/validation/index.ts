56fcc52152451a295a09fb07aec83f39e21cc282
Simlex <simlexafol@gmail.com>
2023-11-05 20:52:37 +0100

    Added sign in validation schema

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,8 +1,13 @@
 import * as z from "zod"
 
-export const signUpValidationSchema = z.object({
+export const SignUpValidationSchema = z.object({
     name: z.string().min(2, {message: "Too Short!"}),
     username: z.string().min(2, {message: "Too Short!"}),
+    email: z.string().email(),
+    password: z.string().min(8, {message: "Password must be at least 8 characters long"}),
+})
+
+export const SignInValidationSchema = z.object({
     email: z.string().email(),
     password: z.string().min(8, {message: "Password must be at least 8 characters long"}),
 })
